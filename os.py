c6afb912fda79bc03894ddb5caa7ea034c3d957f
nandan-unni <asnqln@gmail.com>
2020-05-18 00:03:43 +0530

    Done with DB

--- os.py
+++ os.py
@@ -6,12 +6,15 @@ import random
 import datetime
 import time
 import os
+from os import system, name
 
+# Setting up Database
 from pymongo import MongoClient
 route = MongoClient("mongodb://localhost:27017") # Client : route
 proton = route.proton # Database : proton
 contacts = proton.contacts # Collection : contacts
 notes = proton.notes # Collection : notes
+
 
 def clear():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -27,7 +30,7 @@ def off():
     print("\t*---*---*---*")
     print("\t     \ /     ")
     print("\t      *      ")
-    time.sleep(0.8)
+    time.sleep(0.5)
     clear()
     exit()
 
@@ -36,18 +39,12 @@ def off():
 def addcontact():
     clear()
     print("\n\n\tA D D   C O N T A C T S")
-    f=open("contacts.txt","a")
     cname=input("\n\tName : ")
-    f.write(cname)
-    f.write("\n")
     cphno=input("\tPhone Number : ")
-    f.write(cphno)
-    f.write("\n")
-    f.close()
     clear()
     person = {
-        'Name' : cname,
-        'Phno' : cphno
+        'name' : cname,
+        'phno' : cphno
     }
     contacts.insert_one(person)
     a=int(input("\n\t1. Add Contact\t2. Back\n\n\t    Menu : "))
@@ -59,17 +56,8 @@ def addcontact():
 def dispcontact():
     clear()
     print("\n\n\t\tC O N T A C T S\n")
-    f=open("contacts.txt","r")
-    contacts=f.readlines()
-    j=0
-    for i in contacts:
-        if j%2==0:
-            print(i,end="")
-        else:
-            print(i)
-        j=j+1
-    f.close()
-    del contacts
+    for contact in contacts.find().sort('name', 1):
+        print(contact['name'], ' : ', contact['phno'])
     input("\n\t    Continue... ")
 
 def editcontact():
@@ -79,10 +67,10 @@ def editcontact():
     c=int(input("\n\t1. Edit Name\n\t2. Edit phone number\n\t    Menu : "))
     if c==1:
         d=input("\n\tNew Name : ")
-        contacts.update_one({'Name' : a}, {'$set': {'Name' : d}})
+        contacts.update_one({'name' : a}, {'$set': {'name' : d}})
     else:
         d=input("\n\tNew Phone Number : ")
-        contacts.update_one({'Name' : a}, {'$set': {'Phno' : d}})
+        contacts.update_one({'name' : a}, {'$set': {'phno' : d}})
     clear()
     return None
 
@@ -90,7 +78,7 @@ def deletecontact():
     dispcontact()
     print("\n\n\t D E L E T E   C O N T A C T")
     a=input("\n\n\tName : ")
-    contacts.delete_one({'Name' : a})
+    contacts.delete_one({'name' : a})
     return None
 
 ########################### N O T E S #########################
@@ -102,8 +90,8 @@ def addnotes():
     ntext=input("\n Type notes and press enter to save : \n\n    ")
     clear()
     note = {
-        'Title' : ntitle,
-        'Text' : ntext
+        'title' : ntitle,
+        'text' : ntext
     }
     notes.insert_one(note)
     a=int(input("\n\t1. Add Notes\t2. Back\n\n\t    Menu : "))
@@ -112,7 +100,20 @@ def addnotes():
     else:
         return None
 
-# def delete
+def dispnotes():
+    clear()
+    print("\n\n\t\tC O N T A C T S\n")
+    for note in notes.find().sort('title', 1):
+        print(note['title'])
+        print("\n", note['text'], "\n\n")
+
+def deletenotes():
+    clear()
+    print("\n\n\tD E L E T E   N O T E S")
+    a=input("\n\n\tTitle : ")
+    notes.delete_one({'title' : a})
+    return None
+
 ###################### C A L C U L A T O R ####################
 
 def calculator():
@@ -241,23 +242,34 @@ def hangman():
         else:
             return None
 
+########################## HANGMAN GAME #######################
+
+def reset():
+    clear()
+    print("\n\n")
+    print("Deleting Data...", end="")
+    time.sleep(0.5)
+    contacts.delete_many({})
+    notes.delete_many({})
+    return None
 ############################ HOME #############################
 
 def home():
-    contacts = 1
     clear()
     print("\n\n")
     clock=datetime.datetime.now()
     print("\t\t",clock.day,"/",clock.month,"/",clock.year)
     print("\t\t   ",clock.hour,":",clock.minute)
-    print("\n\n  1. CONTACTS\t2. NOTES\t3. CALCULATOR\n\n  4. GAMES\t5. REFRESH\t6. POWER OFF")
+    print("\n\n  1. CONTACTS\t2. NOTES\t3. CALCULATOR\n\n  4. GAMES\t5. REFRESH\t6. SETTINGS\n\n  7. POWER OFF")
     a=int(input("\n\tMenu : "))
     b=int()
+
     if a==1:
         clear()
         print("\n\n\tC O N T A C T S")
         print("\n\n  1. Add Contact\t2. View Contacts\n\n  3. Edit Contact\t4. Delete Contact\n\n  5. Back")
         b=int(input("\n\tMenu : "))
+
         if b==1:
             addcontact()
             home()
@@ -272,21 +284,28 @@ def home():
             home()
         else:
             home()
+        
     elif a==2:
         clear()
         print("\n\n\t N O T E S")
-        b=int(input("\n\n 1. Add Notes\t2. View Notes\n\n 3. Back\n\n\tMenu : "))
+        b=int(input("\n\n 1. Add Notes\t2. View Notes\n\n 3. Delete Notes\t4. Back\n\n\tMenu : "))
         if b==1:
             addnotes()
             home()
         elif b==2:
+            dispnotes()
+            home()
+        elif b==3:
+            deletenotes()
             home()
         else:
-            home
+            home()
+    
     elif a==3:
         clear()
         calculator()
         home()
+    
     elif a==4:
         clear()
         print("\n\n  1. Hangman\t2. Back")
@@ -298,11 +317,27 @@ def home():
             home()
         else:
             home()
+    
     elif a==5:
         home()
+    
     elif a==6:
         clear()
+        a=int(input("\n\n 1. Storage\t2. Factory Reset\t3. Back\n\n\tMenu : "))
+        if a==1:
+            home()
+        elif a==2:
+            reset()
+            home()
+        elif a==3:
+            home()
+        else:
+            home()
+    
+    elif a==7:
+        clear()
         off()
+    
     else:
         home()
 
